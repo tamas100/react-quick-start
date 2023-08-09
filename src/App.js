3657a2ef3c4209a8d4eb587b463943b52927e214
Tamás Kis <t.kis502@gmail.com>
2023-08-09 11:05:52 +0200

    add TextPractice component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MyButton from './Components/MyButton/MyButton';
+import TextPractice from './Components/TextPractice/TextPractice';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <header className="App-header">
         <h1>React Quick Start</h1>
         <MyButton />
+        <TextPractice />
       </header>
     </div>
   );
---
+++ src/Components/TextPractice/TextPractice.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TextPractice() {
+  return (
+    <>
+      <h2>Some text about anything</h2>
+      <p>How can I help you?</p>
+    </>
+  );
+}
