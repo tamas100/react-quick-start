65dca13621eacc6ae6b9594aec16b1f1e8e08cf8
Tamás Kis <t.kis502@gmail.com>
2023-08-09 11:18:07 +0200

    add TextPractice.css

--- src/Components/TextPractice/TextPractice.jsx
+++ src/Components/TextPractice/TextPractice.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './TextPractice.css'
 
 export default function TextPractice() {
   return (
     <>
       <h2>Some text about anything</h2>
-      <p>How can I help you?</p>
+      <p className='red-text'>How can I help you?</p>
     </>
   );
 }
