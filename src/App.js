5cc8eb77e7f0254d0f35cf661ac22704ded25122
Tamás Kis <t.kis502@gmail.com>
2023-08-09 10:00:22 +0200

    add MyButton component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MyButton from './Components/MyButton/MyButton';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Quick Start</h1>
+        <MyButton />
       </header>
     </div>
   );
---
+++ src/Components/MyButton/MyButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MyButton() {
+  return (
+    <button>
+      Push!
+    </button>
+  );
+}
