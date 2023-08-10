0d5160d672eaf5fa36392e508931924f1b11ea13
Tamás Kis <t.kis502@gmail.com>
2023-08-10 10:07:25 +0200

    add images to CarProfile.jsx

--- src/Components/CarProfile/CarProfile.jsx
+++ src/Components/CarProfile/CarProfile.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import './CarProfile.css';
 
 const car = [{
     manufacturer: 'Honda',
     type: 'Civic',
     fuel: 'petrol',
-    regNumber: 'MTD-562'
+    regNumber: 'MTD-562',
+    picture: 'https://www.driva.com.au/static/9426e1fb4f6ff994c46b4409b8bc6156/01c9c/8c484284-920b-4953-9d6e-f03722109839_2023-Honda-Civic-Type-R-front.webp'
   },
   {
     manufacturer: 'Kia',
     type: 'Ceed',
     fuel: 'petrol',
-    regNumber: 'AAFE-555'
+    regNumber: 'AAFE-555',
+    picture: 'https://www.diariomotor.com/imagenes/2012/10/kia-ceed-ficha-dm-p-61df223f386b4.jpg'
   },
   {
     manufacturer: 'Toyota',
     type: 'Corolla',
     fuel: 'hybrid',
-    regNumber: 'ABAC-256'
+    regNumber: 'ABAC-256',
+    picture: 'https://di-enrollment-api.s3.amazonaws.com/toyota/models/2023/corolla-hybrid/Trims/2023+Hybrid+LE.png'
   }
   
 ];
@@ -24,7 +28,7 @@ const car = [{
 export default function CarProfile() {
     const listItems = car.map(car =>
         <li key={car.regNumber}>
-            {car.manufacturer}
+            {car.manufacturer} - {car.fuel} <img className='car-list-img' src={car.picture} alt={car.manufacturer} />
         </li>
         );
     return (
@@ -34,6 +38,7 @@ export default function CarProfile() {
             <p>
                 {car[0].fuel}
             </p>
+            <img className='car-img' src={car[0].picture} alt="Honda Civic 2023" />
             <ul>{listItems}</ul>
         </>
     );
