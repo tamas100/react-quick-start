a677fd4426f70f3b4d52b213ccf507bf23477355
Tamás Kis <t.kis502@gmail.com>
2023-08-10 17:42:43 +0200

    add inline style like an object to CarConditional.jsx

--- src/Components/CarConditional/CarConditional.jsx
+++ src/Components/CarConditional/CarConditional.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import CarProfile from '../CarProfile/CarProfile';
 
+const styles = {  
+  headOne: {
+    color: '#FFD066'
+  }
+};
 
 export default function CarConditional({myCar}) {
   return (
     <>
+      <h1 style={styles.headOne}>Honda, vagy Kia?</h1>
       {
         myCar === 'Honda' ? <CarProfile/> :
-         <p style={
+        <p style={
           {color: 'lightgreen', fontWeight: 'bold'}
         }
         >Kia? Bad decision!</p>
